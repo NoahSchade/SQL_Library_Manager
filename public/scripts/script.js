d52a6ca2adc6c8f66be7cfbde2fe7ed20c226083
Noah Schade <noah9@mail.com>
2019-06-28 18:00:43 -0400

    Removed unnecessary code

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,17 +6,17 @@ let match2;
 
 
 function searchTitle(event) { 
+  
   searchForTitle = event.target.value.toLowerCase();
-  console.log(document.querySelectorAll('.select-title').length);
+
   for(let i = 0; i < document.querySelectorAll('.select-title').length; i++) {
 
     titleList = document.querySelectorAll('.select-title')[i].textContent.toLowerCase();
 
     let regex = new RegExp("^" + searchForTitle + ".*");
-        match1 = searchForTitle.match(regex);
-        match2 = titleList.match(regex);
+        match = titleList.match(regex);
 
-    if(match1 && match2){
+    if(match){
       document.querySelectorAll('.selectRow')[i].style.display = '';
     } else {
       document.querySelectorAll('.selectRow')[i].style.display = 'none';
