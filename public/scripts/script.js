ae0b26ded091a0501e970620df3fa1e6640bb6ba
Noah Schade <noah9@mail.com>
2019-06-29 10:42:35 -0400

    Updated search feature. Still not completely functional

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,73 +1,89 @@
+let counterArrayGlobal = [];
+
 let searchForTitle;
 let titleList;
 let matchTitle;
 let regexTitle;
 let counterArrayTitle = [];
 
-function searchTitle(event) {
-  searchForTitle = event.target.value.toLowerCase();
+function searchMashup(event) {
 
-  for(let i = 0; i < document.querySelectorAll('.select-title').length; i++) {
+  if(event.target.id === 'search-title') {
+    searchForTitle = event.target.value.toLowerCase();
 
-    titleList = document.querySelectorAll('.select-title')[i].textContent.toLowerCase();
+    for(let i = 0; i < document.querySelectorAll('.select-title').length; i++) {
 
-    regexTitle = new RegExp("^" + searchForTitle + ".*");
-    matchTitle = titleList.match(regexTitle);
+      titleList = document.querySelectorAll('.select-title')[i].textContent.toLowerCase();
 
-    if(matchTitle){
-      selectRow = document.querySelectorAll('.selectRow');
-      selectRow[i].style.display = '';
-      counterArrayTitle.push(selectRow[i]);
-    } else {
-      document.querySelectorAll('.selectRow')[i].style.display = 'none';
-    }
-  } 
-  for(let x = 0; x < counterArrayTitle.length; x += 2){
-    counterArrayTitle[x].style.backgroundColor = '#E7E7E7';
+      regexTitle = new RegExp("^" + searchForTitle + ".*");
+      matchTitle = titleList.match(regexTitle);
+
+      if(matchTitle){
+        selectRow = document.querySelectorAll('.selectRow');
+        // selectRow[i].style.display = '';
+        counterArrayTitle.push(selectRow[i]);
+        counterArrayGlobal.push(selectRow[i]);
+      } else {
+        document.querySelectorAll('.selectRow')[i].style.display = 'none';
+      }
+    } 
   }
 
-  for(let y = 1; y < counterArrayTitle.length; y += 2){
-    counterArrayTitle[y].style.backgroundColor = 'white';
-  }
-
-  counterArrayTitle = [];
-
-}
 
 /////////////////////////////////////////////////////////////////////////////////
 
-let searchForAuthor;
-let authorList;
-let matchAuthor;
-let regexAuthor;
-let counterArrayAuthor = [];
+  let searchForAuthor;
+  let authorList;
+  let matchAuthor;
+  let regexAuthor;
+  let counterArrayAuthor = [];
 
-function searchAuthor(event) {
-  searchForAuthor = event.target.value.toLowerCase();
+  if(event.target.id === 'search-author') {
+    searchForAuthor = event.target.value.toLowerCase();
 
-  for(let i = 0; i < document.querySelectorAll('.select-author').length; i++) {
+    for(let i = 0; i < document.querySelectorAll('.select-author').length; i++) {
 
-    authorList = document.querySelectorAll('.select-author')[i].textContent.toLowerCase();
+      authorList = document.querySelectorAll('.select-author')[i].textContent.toLowerCase();
 
-    regexAuthor = new RegExp("^" + searchForAuthor + ".*");
-    matchAuthor = authorList.match(regexAuthor);
+      regexAuthor = new RegExp("^" + searchForAuthor + ".*");
+      matchAuthor = authorList.match(regexAuthor);
 
-    if(matchAuthor){
-      selectRow = document.querySelectorAll('.selectRow');
-      selectRow[i].style.display = '';
-      counterArrayAuthor.push(selectRow[i]);
-    } else {
-      document.querySelectorAll('.selectRow')[i].style.display = 'none';
+      if(matchAuthor){
+        selectRow = document.querySelectorAll('.selectRow');
+        // selectRow[i].style.display = '';
+        counterArrayAuthor.push(selectRow[i]);
+        counterArrayGlobal.push(selectRow[i]);
+      } else {
+        document.querySelectorAll('.selectRow')[i].style.display = 'none';
+      }
     }
-  } 
-  for(let x = 0; x < counterArrayAuthor.length; x += 2){
-    counterArrayAuthor[x].style.backgroundColor = '#E7E7E7';
   }
 
-  for(let y = 1; y < counterArrayAuthor.length; y += 2){
-    counterArrayAuthor[y].style.backgroundColor = 'white';
+  for(let x = 0; x < counterArrayGlobal.length; x += 2){
+    counterArrayGlobal[x].style.backgroundColor = '#E7E7E7';
   }
 
-  counterArrayAuthor = [];
+  for(let y = 1; y < counterArrayGlobal.length; y += 2){
+    counterArrayGlobal[y].style.backgroundColor = 'white';
+  }
 
+  for(let x = 0; x < counterArrayGlobal.length; x++){
+    for(let j = 0; j < counterArrayGlobal.length; j++){
+      if(counterArrayGlobal[x] === counterArrayTitle[j]){
+        counterArrayGlobal[x].style.display = '';
+
+        for(let z = 0; z < counterArrayTitle.length; z += 2){
+          counterArrayTitle[z].style.backgroundColor = '#E7E7E7';
+        }
+      
+        for(let y = 1; y < counterArrayTitle.length; y += 2){
+          counterArrayTitle[y].style.backgroundColor = 'white';
+        }
+      } else {
+        
+      }
+    }
+  }
+  
+  counterArrayGlobal = [];
 }
