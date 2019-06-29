d191d102c4426a7f219c8021fa9712c5e4b71be8
Noah Schade <noah9@mail.com>
2019-06-28 22:00:05 -0400

    Fixed the background color of the list items when making a search.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,25 +1,35 @@
 let searchForTitle;
 let titleList;
-let titleListRegex;
-let match1;
-let match2;
+let match;
+let regex;
+let counterArray = [];
 
-
-function searchTitle(event) { 
-  
+function searchTitle(event) {
   searchForTitle = event.target.value.toLowerCase();
 
   for(let i = 0; i < document.querySelectorAll('.select-title').length; i++) {
 
     titleList = document.querySelectorAll('.select-title')[i].textContent.toLowerCase();
 
-    let regex = new RegExp("^" + searchForTitle + ".*");
-        match = titleList.match(regex);
+    regex = new RegExp("^" + searchForTitle + ".*");
+    match = titleList.match(regex);
 
     if(match){
-      document.querySelectorAll('.selectRow')[i].style.display = '';
+      selectRow = document.querySelectorAll('.selectRow');
+      selectRow[i].style.display = '';
+      counterArray.push(selectRow[i]);
     } else {
       document.querySelectorAll('.selectRow')[i].style.display = 'none';
     }
+  } 
+  for(let x = 0; x < counterArray.length; x += 2){
+    counterArray[x].style.backgroundColor = '#E7E7E7';
   }
+
+  for(let y = 1; y < counterArray.length; y += 2){
+    counterArray[y].style.backgroundColor = 'white';
+  }
+
+  counterArray = [];
+
 }
