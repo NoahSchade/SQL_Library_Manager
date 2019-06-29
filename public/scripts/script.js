36b2f73e20450a35e6a30f24dde0524ca6a5c0ed
Noah Schade <noah9@mail.com>
2019-06-28 23:36:44 -0400

    Renamed variables for title search feature.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,8 @@
 let searchForTitle;
 let titleList;
-let match;
-let regex;
-let counterArray = [];
+let matchTitle;
+let regexTitle;
+let counterArrayTitle = [];
 
 function searchTitle(event) {
   searchForTitle = event.target.value.toLowerCase();
@@ -11,25 +11,25 @@ function searchTitle(event) {
 
     titleList = document.querySelectorAll('.select-title')[i].textContent.toLowerCase();
 
-    regex = new RegExp("^" + searchForTitle + ".*");
-    match = titleList.match(regex);
+    regexTitle = new RegExp("^" + searchForTitle + ".*");
+    matchTitle = titleList.match(regexTitle);
 
-    if(match){
+    if(matchTitle){
       selectRow = document.querySelectorAll('.selectRow');
       selectRow[i].style.display = '';
-      counterArray.push(selectRow[i]);
+      counterArrayTitle.push(selectRow[i]);
     } else {
       document.querySelectorAll('.selectRow')[i].style.display = 'none';
     }
   } 
-  for(let x = 0; x < counterArray.length; x += 2){
-    counterArray[x].style.backgroundColor = '#E7E7E7';
+  for(let x = 0; x < counterArrayTitle.length; x += 2){
+    counterArrayTitle[x].style.backgroundColor = '#E7E7E7';
   }
 
-  for(let y = 1; y < counterArray.length; y += 2){
-    counterArray[y].style.backgroundColor = 'white';
+  for(let y = 1; y < counterArrayTitle.length; y += 2){
+    counterArrayTitle[y].style.backgroundColor = 'white';
   }
 
-  counterArray = [];
+  counterArrayTitle = [];
 
 }
