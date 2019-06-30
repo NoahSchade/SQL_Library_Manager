beef5a5d652d7d9937104db14c32d3a0273eb9dc
Noah Schade <noah9@mail.com>
2019-06-29 23:37:10 -0400

    Removed unnecessary code.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,23 +1,19 @@
-let counterArrayGlobal = [];
 let selectRow = document.querySelectorAll('.selectRow');
-
 
 let searchForTitle;
 let titleList;
 let matchTitle;
 let regexTitle;
-let counterArrayTitle = [];
 let titleSearch = document.querySelector('#search-title');
 
 let searchForAuthor;
 let authorList;
 let matchAuthor;
 let regexAuthor;
-let counterArrayAuthor = [];
 let authorSearch = document.querySelector('#search-author');
 
 
-function searchMashup(event) {
+function searchMashup() {
 
   searchForTitle = titleSearch.value.toLowerCase();
   searchForAuthor = authorSearch.value.toLowerCase();
@@ -31,8 +27,6 @@ function searchMashup(event) {
 
     if(matchTitle){
       selectRow[i].style.display = '';
-      counterArrayTitle.push(selectRow[i]);
-      counterArrayGlobal.push(selectRow[i]);
     } else {
       selectRow[i].style.display = 'none';
     }
@@ -44,40 +38,9 @@ function searchMashup(event) {
     matchAuthor = authorList.match(regexAuthor);
 
     if(matchAuthor){
-      // selectRow[i].style.display = '';
-      counterArrayAuthor.push(selectRow[i]);
-      counterArrayGlobal.push(selectRow[i]);
+
     } else {
       selectRow[i].style.display = 'none';
     }
   } 
 }
-
-  // for(let x = 0; x < counterArrayGlobal.length; x += 2){
-  //   counterArrayGlobal[x].style.backgroundColor = '#E7E7E7';
-  // }
-
-  // for(let y = 1; y < counterArrayGlobal.length; y += 2){
-  //   counterArrayGlobal[y].style.backgroundColor = 'white';
-  // }
-
-  // for(let x = 0; x < counterArrayGlobal.length; x++){
-  //   for(let j = 0; j < counterArrayGlobal.length; j++){
-  //     if(counterArrayGlobal[x] === counterArrayTitle[j]){
-  //       counterArrayGlobal[x].style.display = '';
-
-  //       for(let z = 0; z < counterArrayTitle.length; z += 2){
-  //         counterArrayTitle[z].style.backgroundColor = '#E7E7E7';
-  //       }
-      
-  //       for(let y = 1; y < counterArrayTitle.length; y += 2){
-  //         counterArrayTitle[y].style.backgroundColor = 'white';
-  //       }
-  //     } else {
-        
-  //     }
-  //   }
-  
-  
-  // counterArrayGlobal = [];
-// }
