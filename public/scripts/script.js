8c840302a3e016f9565b51a3c90549fcdd3ed150
Noah Schade <noah9@mail.com>
2019-07-01 18:18:49 -0400

    Added buttons. Incomplete.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -91,31 +91,40 @@ function searchMashup() {
   evenArray = [];
 }
 
-let table = document.querySelector("table");
+// let table = document.querySelector("table");
 let appendButtons = document.querySelector(".appendButtons");
-let button_1 = document.createElement("BUTTON");
-let button_2 = document.createElement("BUTTON");
-let button_3 = document.createElement("BUTTON");
-appendButtons.appendChild(button_1);
-appendButtons.appendChild(button_2);
-appendButtons.appendChild(button_3);
-appendButtons.style.display = "block";
-appendButtons.style.margin = "auto";
-button_1.style.height = "40px";
-button_1.style.width = "40px";
-button_2.style.height = "40px";
-button_2.style.width = "40px";
-button_3.style.height = "40px";
-button_3.style.width = "40px";
-button_1.style.display = "block";
-button_2.style.display = "block";
-button_3.style.display = "block";
-button_1.style.position = "relative";
-button_2.style.position = "relative";
-button_3.style.position = "relative";
-button_1.style.float = "left";
-button_2.style.float = "left";
-button_3.style.float = "left";
-button_1.style.margin = "auto";
-button_2.style.margin = "auto";
-button_3.style.margin = "auto";
+// let button_1 = document.createElement("BUTTON");
+// let button_2 = document.createElement("BUTTON");
+// let button_3 = document.createElement("BUTTON");
+// appendButtons.appendChild(button_1);
+// appendButtons.appendChild(button_2);
+// appendButtons.appendChild(button_3);
+// appendButtons.style.display = "block";
+// appendButtons.style.margin = "auto";
+// button_1.style.height = "40px";
+// button_1.style.width = "40px";
+// button_2.style.height = "40px";
+// button_2.style.width = "40px";
+// button_3.style.height = "40px";
+// button_3.style.width = "40px";
+// button_1.style.display = "block";
+// button_2.style.display = "block";
+// button_3.style.display = "block";
+// button_1.style.position = "relative";
+// button_2.style.position = "relative";
+// button_3.style.position = "relative";
+// button_1.style.float = "left";
+// button_2.style.float = "left";
+// button_3.style.float = "left";
+// button_1.style.margin = "auto";
+// button_2.style.margin = "auto";
+// button_3.style.margin = "auto";
+
+console.log(selectRow.length);
+let button = [];
+
+for(let i = 0; i < (Math.ceil(selectRow.length / 10) * 10); i += 10){
+  appendButtons.appendChild(document.createElement("BUTTON"));
+}
+
+
