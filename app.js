f145a268464c254e3d4b164187e3fafcc0ac783e
Noah Schade <noah9@mail.com>
2019-07-03 11:18:17 -0400

    Modified code comments.

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// setup
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(logger('dev'));
