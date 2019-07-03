10ca2a53faee1710a32583fdaea68a3caeef775c
Noah Schade <noah9@mail.com>
2019-07-03 13:39:56 -0400

    Removed unnecessary code and added code comments.

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,20 @@
+// Setting dependencies.
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.redirect("/books")
 });
 
+// If url path is incorrect, show 'page-not-found' page.
 router.get(/^(?!.*\/books)/, function(req, res, next) {
   res.render('books/page-not-found');
 });
 
+// If the url path is '/books/books/new' redirect to '/books/new'.
 router.get(/^((\/books\/books\/new).*)$/, function(req, res, next) {
   res.redirect("/books/new")
 });
-
-// router.get(/^((?!\/books).)+|((\/books\/books).*)$/, function(req, res, next) {
-//   res.redirect("/books")
-// });
 
 module.exports = router;
